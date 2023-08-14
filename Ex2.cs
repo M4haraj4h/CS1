ae8d35330976e84f1a1fb291caf812af36cb2428
«1mitati0n» <ssasbadmood@yandex.ru>
2023-08-15 01:48:08 +0700

    ex2

---
+++ Ex2.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a>b)
+{
+    Console.WriteLine($"Наибольшее число: {a}");
+    Console.WriteLine($"Наименьшее число: {b}");
+}
+else if (a<b)
+{
+    Console.WriteLine($"Наибольшее число: {b}");
+    Console.WriteLine($"Наименьшее число: {a}");
+}
+else if (a==b)
+{
+    Console.WriteLine("Числа одинаковы");
+}
