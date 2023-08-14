0515e01f926f823555a97610f440ad909cbb6ea8
«1mitati0n» <ssasbadmood@yandex.ru>
2023-08-15 02:14:12 +0700

    ex4

---
+++ Ex4.cs
@@ -0,0 +1,21 @@
+﻿int[] a=new int[3];
+int max = 0;
+
+for(int i = 0;i<a.Length;i++)
+{
+    a[i]= Convert.ToInt32(Console.ReadLine());
+}
+
+for(int i = 0;i<a.Length;i++)
+{
+    if(a[i]>max)
+        {
+            max = a[i];
+            i++;
+        }
+    else
+        i++;
+}
+
+Console.WriteLine("Наибольшее число: ");
+Console.WriteLine($"{max}");
